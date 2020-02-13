301587d7d1d03deaaeb89d32e8dbb807ac428c13
Luke Patton <pattonluke2014@gmail.com>
2020-02-13 16:19:58 -0500

    First part of Login.

--- app/src/main/java/com/example/mycampus_application/LoginActivity.java
+++ app/src/main/java/com/example/mycampus_application/LoginActivity.java
@@ -1,18 +1,103 @@
 package com.example.mycampus_application;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
 
+    EditText emailField;
+    EditText passwordField;
+
+    Intent nextActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        // TextViews
+        emailField = findViewById(R.id.signin_email);
+        passwordField = findViewById(R.id.signin_password);
+
+
     }
+
+    private void signIn(String email, String password) {
+
+        //Validate Email
+        if (!validateForm()) {
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //sign in successful
+                            startActivity(nextActivity);
+
+                        } else {
+                            //sign in failed
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+
+    }
+
+    public void startSignIn (View view) {
+        signIn(emailField.getText().toString(), passwordField.getText().toString());
+        nextActivity = new Intent(this, HomeActivity.class);
+    }
+
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = emailField.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            emailField.setError("Required.");
+            valid = false;
+        } else {
+            if (email.endsWith("@umich.edu")) {
+                emailField.setError(null);
+            } else {
+                emailField.setError("Enter a valid umich email address.");
+                valid = false;
+            }
+        }
+
+        String password = passwordField.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            passwordField.setError("Required.");
+            valid = false;
+        } else {
+            passwordField.setError(null);
+        }
+
+        return valid;
+    }
+
+
 }
