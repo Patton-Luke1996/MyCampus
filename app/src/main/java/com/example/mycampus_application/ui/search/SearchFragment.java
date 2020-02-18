12b4aab37c788bd30bfe7b8dfc29dac887170a43
christopher <wrightcr@umich.edu>
2020-02-18 03:21:12 -0500

    name changes

--- app/src/main/java/com/example/mycampus_application/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/mycampus_application/ui/search/SearchFragment.java
@@ -18,23 +18,38 @@ import com.example.mycampus_application.R;
 
 public class SearchFragment extends Fragment {
 
-    private SearchViewModel slideshowViewModel;
+    private SearchViewModel searchViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
+
+
+        searchViewModel =
                 ViewModelProviders.of(this).get(SearchViewModel.class);
         View root = inflater.inflate(R.layout.fragment_search, container, false);
         final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(this, new Observer<String>() {
+
+        Spinner spinner = root.findViewById(R.id.spinner_category);
+        ArrayAdapter<CharSequence> adapter =
+                ArrayAdapter.createFromResource(this.getActivity(), R.array.array_category,
+                        android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
+        searchViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
         });
+
+
         return root;
 
 
 
     }
+
+
 }
