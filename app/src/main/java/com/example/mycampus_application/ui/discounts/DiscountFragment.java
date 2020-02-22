5169eb463a25c597d2e7804fbbac79a44e185dbb
christopher <wrightcr@umich.edu>
2020-02-22 17:35:16 -0500

    ui for profile

--- app/src/main/java/com/example/mycampus_application/ui/discounts/DiscountFragment.java
+++ app/src/main/java/com/example/mycampus_application/ui/discounts/DiscountFragment.java
@@ -11,6 +11,8 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mycampus_application.R;
 
@@ -18,11 +20,23 @@ public class DiscountFragment extends Fragment {
 
     private DiscountViewModel discountViewModel;
 
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         discountViewModel =
                 ViewModelProviders.of(this).get(DiscountViewModel.class);
         View root = inflater.inflate(R.layout.fragment_discount, container, false);
+
+        recyclerView = root.findViewById(R.id.discount_recycler);
+        recyclerView.setHasFixedSize(true);
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this.getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+       // mAdapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
 
         final TextView textView = root.findViewById(R.id.text_discount);
         discountViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
