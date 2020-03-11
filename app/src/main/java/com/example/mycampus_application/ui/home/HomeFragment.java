f08648b7e5105bcf2388a910e31135aceca7a33b
christopher <wrightcr@umich.edu>
2020-03-11 00:44:20 -0400

    home recyclerview adapted

--- app/src/main/java/com/example/mycampus_application/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/mycampus_application/ui/home/HomeFragment.java
@@ -11,6 +11,8 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mycampus_application.R;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -18,8 +20,19 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
+
+    private ArrayList<String> mItemName = new ArrayList<>();
+    private ArrayList<String> mImages = new ArrayList<>();
+    private ArrayList<String> mQty = new ArrayList<>();
+    private ArrayList<String> mPrice = new ArrayList<>();
+    private ArrayList<String> mDescription = new ArrayList<>();
+    private ArrayList<String> mCategory = new ArrayList<>();
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView recyclerView;
 
     private HomeViewModel homeViewModel;
 
@@ -37,6 +50,16 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+
+        initRecyclerView();
+
+        recyclerView = root.findViewById(R.id.homeRecycler);
+        layoutManager = new LinearLayoutManager(this.getActivity());
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+        mAdapter = new HomeRecyclerAdapter(mItemName,mImages,mQty,mPrice,
+                mDescription, mCategory, this.getContext());
+        recyclerView.setAdapter(mAdapter);
 
         db = FirebaseFirestore.getInstance();
 
@@ -61,5 +84,17 @@ public class HomeFragment extends Fragment {
                         }
                     }
                 });
+    }
+
+
+    private void initRecyclerView()
+    {
+        mItemName.add("Purple Couch");
+        mCategory.add("Furniture");
+        mDescription.add("A purple couch that is in decent condition");
+        mPrice.add("150");
+        mQty.add("1");
+        mImages.add("https://5.imimg.com/data5/BC/SG/MY-24423473/sofa-set-500x500.jpg");
+
     }
 }
